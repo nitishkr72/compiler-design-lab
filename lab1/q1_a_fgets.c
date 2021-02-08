b96733c03abd373ea5cd51c43ea46fbfe1280d48
Nitish Kumar <nitish_tu@outlook.com>
2021-02-08 21:58:03 +0530

    Create q1_a_fgets.c

---
+++ lab1/q1_a_fgets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BUFF_SIZE 100
+
+int main() {
+	FILE * infile;
+	char fname[15] = "lab1.txt";
+	infile = fopen(fname, "r");
+
+	int line=0;
+	char rline[BUFF_SIZE];
+	while(fgets(rline, BUFF_SIZE, infile))
+		printf("%d %s", ++line, rline);
+
+	return 0;
+}
