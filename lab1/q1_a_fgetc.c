4382105ed167201233dd516f770e9f05360edbf2
Nitish Kumar <nitish_tu@outlook.com>
2021-02-09 12:16:29 +0530

    Update q1_a_fgetc.c

--- lab1/q1_a_fgetc.c
+++ lab1/q1_a_fgetc.c
@@ -6,17 +6,20 @@ int main() {
 	char fname[15] = "lab1.txt";
 	infile = fopen(fname, "r");
 
-	int line=0;
-
-	char c = fgetc(infile);
-	printf("%d ", ++line);
-	while(c != EOF) {
-		printf("%c", c);
-		if(c == '\n' && (c = fgetc(infile)) != EOF)
-			printf("%d ", ++line);
-		else
-			c = fgetc(infile);
+	if(infile) {
+		int line=0;
+		char c = fgetc(infile);
+		printf("%d ", ++line);
+		while(c != EOF) {
+			printf("%c", c);
+			if(c == '\n' && (c = fgetc(infile)) != EOF)
+				printf("%d ", ++line);
+			else
+				c = fgetc(infile);
+		}
+		fclose(infile);
+	} else {
+		fprintf(stderr, "fopen() failed for '%s'\n", fname);
 	}
-	fclose(infile);
 	return 0;
 }
