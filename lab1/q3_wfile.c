af8537c9f777d08a947e44eb7d125fd320b1c28a
Nitish Kumar <nitish_tu@outlook.com>
2021-02-11 23:15:50 +0530

    Update q3_wfile.c

--- lab1/q3_wfile.c
+++ lab1/q3_wfile.c
@@ -8,25 +8,26 @@ int main() {
 	FILE * infile_dec, * infile_bin;
 	char fname_dec[BUFF_SIZE], fname_bin[BUFF_SIZE];
 
-	char sData[N][BUFF_SIZE];
-
-	printf("Enter Name and roll Number eg.'>> Nitish CSB18040'\n");
-	int i = 0;
-	while(i<N && fgets(sData[i], BUFF_SIZE, stdin))
-		i++;
+	char sData[BUFF_SIZE];
 
 	printf("Enter the filename(saving in text format): ");
 	scanf("%s", fname_dec);
-	infile_dec = fopen(fname_dec, "w");
+	if((infile_dec = fopen(fname_dec, "w")) == NULL)
+		return 1;
 
 	printf("Enter the filename(saving in binary format): ");
 	scanf("%s", fname_bin);
-	infile_bin = fopen(fname_bin, "wb");
+	if((infile_bin = fopen(fname_bin, "wb")) == NULL)
+		return 1;
 
-	i = 0;
-	while(i < N) {
-		fputs(sData[i], infile_dec);
-		fwrite(sData[i], BUFF_SIZE, 1, infile_bin);
+	int i = 0;
+	printf("Enter Name and roll Number eg.'>> Nitish CSB18040'\n");
+	while(i <= N && fgets(sData, BUFF_SIZE, stdin)) {
+		int k = sizeof(sData);
+		sData[k++] = '\n';
+		sData[k++] = '\0';
+		fputs(sData, infile_dec);
+		fwrite(sData, sizeof(sData), 1, infile_bin);
 		i++;
 	}
 
