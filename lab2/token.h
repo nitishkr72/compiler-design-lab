9d9690bd51bc22dc5a4e089afa026674f0865ed5
Nitish Kumar <nitish_tu@outlook.com>
2021-02-18 22:39:20 +0530

    Create token.h

---
+++ lab2/token.h
@@ -0,0 +1,40 @@
+//token.h
+
+#define TOK_STOP 1
+#define TOK_ONE 2
+#define TOK_ADD 3
+#define TOK_SUB 4
+#define TOK_NUM 5
+#define TOK_MULT 6
+#define TOK_TERM 7
+#define TOK_LOAD 8
+#define TOK_STORE 9
+#define TOK_RESULT 10
+#define TOK_TRANS 11
+#define TOK_NEXT 12
+#define TOK_TRIM 13
+#define TOK_DIV 14
+#define TOK_READ 15
+#define TOK_PRINT 16
+#define TOK_COUNT 17
+#define TOK_LIR 18
+#define TOK_IIR 19
+#define TOK_LOOP 20
+#define TOK_AGAIN 21
+
+#define TOK_START 22
+#define TOK_END 23
+#define TOK_LTORG 24
+#define TOK_DS 25
+#define TOK_DC 26
+
+//#define TOK_ 27
+#define TOK_ID 28
+#define TOK_COMMA 29
+#define TOK_OPEN_BRACE 30
+#define TOK_CLOSE_BRACE 31
+#define TOK_SEMICOLON 32
+#define TOK_COLON 33
+
+#define TOK_SPACE 34
+#define TOK_EOF 35
