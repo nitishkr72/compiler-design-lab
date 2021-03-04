c5a91080a4ba1703d2fa85f4d61de59eea2f4c53
Nitish Kumar <nitish_tu@outlook.com>
2021-03-04 23:26:16 +0530

    Create token.h

---
+++ lab3/token.h
@@ -0,0 +1,34 @@
+// token.h
+
+#define TOK_STOP 	1
+#define TOK_ADD 	2
+#define TOK_SUB 	3
+#define TOK_MULT 	4
+#define TOK_LOAD 	5
+#define TOK_STORE 	6
+#define TOK_TRANS 	7
+#define TOK_TRIM 	8
+#define TOK_DIV 	9
+#define TOK_READ 	10
+#define TOK_PRINT 	11
+#define TOK_LIR 	12
+#define TOK_IIR 	13
+#define TOK_LOOP 	14
+
+#define TOK_START 	15
+#define TOK_END 	16
+#define TOK_LTORG 	17
+
+#define TOK_DS 		18
+#define TOK_DC 		19
+
+#define TOK_NUM 	20
+#define TOK_ID 		21
+#define TOK_COMMA 	22
+#define TOK_OPEN_BRACE 	23
+#define TOK_CLOSE_BRACE 24
+#define TOK_SEMICOLON 	25
+#define TOK_COLON 	26
+#define TOK_SPACE 	27
+
+#define TOK_EOF 	28
